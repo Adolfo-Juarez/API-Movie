69751ada5f5b3eb40aae59526282bc55bf971442
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-10-02 13:38:35 -0500

    Commit inicial, resolución de dependencias

---
+++ src/test/java/upchi/api/movie/ApimovieApplicationTests.java
@@ -0,0 +1,13 @@
+package upchi.api.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
