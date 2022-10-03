6cedede88a3ff9c7d59553a25e4a8d6ff8493ca0
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-10-03 11:27:59 -0500

    Eliminacion de Creator.sh y producer

--- src/main/java/upchi/api/movie/producer/services/interfaces/IProducerService.java
+++
@@ -1,8 +0,0 @@
-package upchi.api.movie.producer.services.interfaces;
-
-public interface IProducerService {
-    // Por Hacer:
-    // - Genera las interfaces de lo métodos para los servicios
-
-    // Recuerda que mínimo deberán haber 4 métodos (del protocolo HTTP)
-}
